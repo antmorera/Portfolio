4e077114eb0727da24cd121608b575a7b04313cc
antmorera <ant.morera.lb@gmail.com>
2023-02-04 17:31:53 -0700

    Lesson 5 updated main page

--- lesson4/chamber/js/chamber.js
+++ lesson4/chamber/js/chamber.js
@@ -1,27 +1,33 @@
 function toggleMenu(){
-  // every time the hamburger button is clicked, the primaryNav will have a class of open
   document.getElementById("primaryNav").classList.toggle("open");
    document.getElementById("hamburgerBtn").classList.toggle("open");
  }
- 
  const x = document.getElementById('hamburgerBtn');
- 
  x.onclick=toggleMenu;
  
  
  
- //getting the date: 
  const today = new Date();
- const fullDate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+ const fullDate = new Intl.DateTimeFormat("en-UK", { dateStyle: "full" }).format(
    today
  );
- //now put the current date in the HTML where it says "header-date":
+ 
  document.querySelector(".header-date").innerHTML = fullDate;
  
  
  
- //putting the "last updated" at the bottom of the footer:
- //the following is the string we want to write, including the last time the document was modified: 
  let dateModified = ('Last Modification: ' + document.lastModified);
- //then we search the document by the ID, in this case "date-mod", and put in the string found in dateModified in that spot: 
  document.getElementById("date-mod").innerHTML = dateModified;
+ const year = today.getFullYear();
+ document.querySelector("#copyright-year").innerHTML = year;
+ if (today.getDay() == 1 || today.getDay() == 2){
+   document.querySelector(".banner").classList.toggle("banner-shown");
+ 
+   document.querySelector(".banner-close").addEventListener('click', function() {
+   this.closest(".banner").style.display = "none";
+ });
+ };
+ 
+ 
+ 
+ 
